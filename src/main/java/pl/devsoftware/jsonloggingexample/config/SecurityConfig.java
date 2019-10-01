c6d30757aebf4818c5aa51a0cbab4de3894217e5
Rafał Podkoński <rafal.podkonski@dev-software.pl>
2019-10-01 08:42:09 +0200

    Return 401 Unauthorized when user send wrong login data

--- src/main/java/pl/devsoftware/jsonloggingexample/config/SecurityConfig.java
+++ src/main/java/pl/devsoftware/jsonloggingexample/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package pl.devsoftware.jsonloggingexample.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -15,5 +18,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("user")
                 .password("{noop}password")
                 .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
     }
 }
